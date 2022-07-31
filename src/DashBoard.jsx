8c983ab27e97462308582b69e9d9e8a859be0161
jirawat rongtong <jirawat.guy.rongtong@gmail.com>
2022-07-31 10:03:17 +0700

    add   Dashboard

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './component/Header/Header'
+import './Dashboard.css'
 
 const DashBoard = () => {
     return (
         <div>
+            <div className='Header-section'>
+                <Header />
 
-            <Header />
+            </div>
+
         </div>
     )
 }
--- src/Store Data/Personaldata.jsx
+++ src/Store Data/Personaldata.jsx
@@ -1,53 +1,52 @@
 
 
-
 const PersonalData = [
     {
-        name : 'Jenifer',
+        name: 'Jenifer',
         gender: 'female',
-        age : 28,
-        weight : 40,
-        height : 148,
-        birthday : 22/06/1995,
-        id : 1,
+        age: 28,
+        weight: 40,
+        height: 148,
+        birthday: 22 / 06 / 1995,
+        id: 1,
 
-        
+
     },
     {
-        name : 'Marcus',
+        name: 'Marcus',
         gender: 'male',
-        age : 40,
-        weight : 70,
-        height : 178,
-        birthday : 20/11/1995,
-        id : 2
+        age: 40,
+        weight: 70,
+        height: 178,
+        birthday: 20 / 11 / 1995,
+        id: 2
     },
     {
-        name : 'Mavarick',
+        name: 'Mavarick',
         gender: 'male',
-        age : 55,
-        weight : 65,
-        height : 168,
-        birthday : 18/08/1990,
-        id : 3
+        age: 55,
+        weight: 65,
+        height: 168,
+        birthday: 18 / 08 / 1990,
+        id: 3
     },
     {
-        name : 'Axise',
+        name: 'Axise',
         gender: 'female',
-        age : 20,
-        weight : 69,
-        height : 160,
-        birthday : 14/02/1989,
-        id : 4
+        age: 20,
+        weight: 69,
+        height: 160,
+        birthday: 14 / 02 / 1989,
+        id: 4
     },
     {
-        name : 'JohnDoe',
+        name: 'JohnDoe',
         gender: 'male',
-        age : 35,
-        weight : 60,
-        height : 148,
-        birthday : 21/06/1995,
-        id : 5
+        age: 35,
+        weight: 60,
+        height: 148,
+        birthday: 21 / 06 / 1995,
+        id: 5
     },
-    
+
 ]
